89d55d6cad793e5abcc35d36858b264a58f0ba37
Alan Yang <alanyangart@gmail.com>
2022-06-27 01:32:28 -0700

    add init and utility pkg

---
+++ utility/get_object_size.py
@@ -0,0 +1,42 @@
+from maya import OpenMaya as om
+from maya import cmds
+
+
+# Return average bounding box length of object based on object-space bbox.
+def get_object_size(selection):
+
+    # Check if selection exists in scene and get OM path
+    selectionList = om.MSelectionList()
+    if not cmds.objExists(selection[0]):
+        raise RuntimeError('Object does not exist: {}'.format(selection[0]))
+        return
+
+    om.MGlobal.getSelectionListByName(selection[0], selectionList)
+    selectedPath = om.MDagPath()
+    selectionList.getDagPath(0, selectedPath)
+
+    # Get the transformation matrix of the selected object.
+    transform = om.MFnTransform(selectedPath)
+    m = transform.transformationMatrix()
+
+    # Get the shape directly below the selected transform.
+    selectedPath.extendToShape()
+    fnMesh = om.MFnMesh(selectedPath)
+    bounds = fnMesh.boundingBox()
+
+    # Transform bbox by object matrix
+    bbmin = bounds.min() * m
+    bbmax = bounds.max() * m
+
+    # Calculate dimensions of bbox
+    dimension = [
+        abs(bbmin.x - bbmax.x),
+        abs(bbmin.y - bbmax.y),
+        abs(bbmin.z - bbmax.z)]
+
+    # Return average length of bbox side
+    return (dimension[0] + dimension[1] + dimension[2])/3
+
+# Example usage
+# selected = cmds.ls(sl=1, type='transform')
+# print(get_object_size(selected))
